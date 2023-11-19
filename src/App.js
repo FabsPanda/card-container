0865d49bd0a974baa19231a39e0331ed3a42ed45
FabsPanda <75232387+FabsPanda@users.noreply.github.com>
2023-11-19 19:23:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+/* WARNING: sorry if my code kinda messy */
+// Notes: The typfaces can't be imported properly
+// in this IDE, idk why, but it works perfectly fine
+// in VSCode. Thank You!
 
-function App() {
+import "./styles.css";
+import { useState } from "react";
+
+export default function ListingAd({
+  pic,
+  title,
+  address,
+  description,
+  project_type,
+  year,
+  ownership_type,
+  logo,
+  availabilities_label,
+  psf_min,
+  psf_max,
+  subprice_label,
+}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isPhoneNumberRevealed, setIsPhoneNumberRevealed] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const revealPhoneNumber = () => {
+    setIsPhoneNumberRevealed((prev) => !prev);
+  };
+
+  // To be honest, I was a bit puzzled about this part to display the description value with newline
+  // '\n' within a single string variable => 'description'. So, I had to do a bit of Googling. :*)
+  // BUT! the rests are smoothly done :)
+
+  const descriptionArray = description.split("\n").map((line, index, array) => {
+    const isPhoneNumber = line.match(/\b(\d{4}\s?\d{4})\b/g);
+
+    return (
+      <span key={index}>
+        {isPhoneNumber ? (
+          <span>
+            {line.split(/(\b\d{4}\s?\d{4}\b)/g).map((part, partIndex) => {
+              if (part.match(/\b(\d{4}\s?\d{4})\b/g)) {
+                return (
+                  <span
+                    key={`${index}-${partIndex}`}
+                    style={{
+                      cursor: "pointer",
+                      textDecoration: "underline",
+                    }}
+                    onClick={revealPhoneNumber}
+                  >
+                    {isPhoneNumberRevealed
+                      ? part
+                      : `${part.substring(0, 4)} XXXX`}
+                  </span>
+                );
+              } else {
+                return part;
+              }
+            })}
+          </span>
+        ) : (
+          line
+        )}
+        {index !== array.length - 1 && <br />}
+      </span>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Card">
+        <img className="mainPic" width="300" height="500" src={pic} />
+        <div className="mainContent">
+          <div className="content1">
+            <div className="left">
+              <div className="title">
+                <img
+                  className="logo"
+                  width={40}
+                  height={40}
+                  src={logo}
+                  alt=""
+                />
+                <div className="foret">
+                  <h1>{title}</h1>
+                  <p className="address">{address}</p>
+                </div>
+              </div>
+
+              <p className="condo">
+                {project_type} · {year} · {ownership_type}
+              </p>
+              <p className="unit">{availabilities_label}</p>
+            </div>
+
+            <div className="right">
+              <div className="price">
+                <h2>
+                  ${psf_min} - ${psf_max} psf
+                </h2>
+                <p>{subprice_label}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="buttonContainer">
+            <button onClick={toggleExpand}>See description</button>
+          </div>
+
+          {isExpanded ? (
+            <div className="wrapper">
+              <p className="description">{descriptionArray}</p>
+            </div>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Importing logo asset from figma :)
+import logo from "./assets/logo.png";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const store = {
+  logo: logo,
+  pic: "https://images.prismic.io/99-content/dc1594fb-f413-44ff-b8b5-c3ba6cd539cd_vbWkNuxt2hjgidVttoMRDX.jpg?auto=compress,format&w=2000&q=100",
+  title: "Forett At Bukit Timah",
+  address: "70 Anchorvale Crescent · D21",
+  project_type: "Condo",
+  year: 2021,
+  ownership_type: "Freehold",
+  psf_min: 2609,
+  psf_max: 3043,
+  subprice_label: "Prices from $1.99M",
+  availabilities_label: "231 units · 1, 2, 3, 4 bedrooms",
+  description: `Phone number: 8234 5678
+
+Superb 3 bedroom in sentosa for a very affordable price and a very nearby bus station.
+
+Please call Alex B (CEA No: R0123456) @  82345678 or the owner (83456789) to arrange for a viewing.`,
+};
+
+ReactDOM.render(
+  <StrictMode>
+    <App {...store} />
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
